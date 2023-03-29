f31027d29757d4b9ee2bdb3a1c082ac0d2941194
AntonioLorusso04 <lorusso.antonio@panettipitagora.edu.it>
2023-03-29 22:50:02 +0200

    2 Update Dropdown.jsx

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import '..style/Dropdown.css'
+import '../styles/Dropdown.css'
 
-function Dropdown(props){
-    return(
-        <div className="Dropdown">
-            <select id={props.id}>
-                {props.values.map((el, index) => {return <option value={el} key={index}>{el}</option>})}
-            </select>
-        </div>
-    )
+function Dropdown(props) {
+
+  return (
+    <div className="Dropdown">
+        <select id={props.id}>
+        {props.values.map((el,index)=> {return <option value={el} key={index}>{el}</option>})}
+        </select>
+    </div>
+  )
 }
 
 export default Dropdown
